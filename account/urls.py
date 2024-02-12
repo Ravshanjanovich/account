68f87f24bb3eb6daeeec595d0b2b28914686c452
Ravshanjanovich <aslbekqurbonboyev12@gmail.com>
2024-02-12 11:20:08 +0500

    start

---
+++ account/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from account import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+
+    #Login in logout
+    path("login/", auth_views.LoginView.as_view(), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
+
+    #Changen password
+    path("password-change/",auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    #Reset password
+    path("password-reset/", auth_views.PasswordChangeView.as_view(), name='password_reset'),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path("password-reset/<uid64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    #Dashboard
+    path('',views.dashboard, name='dashboard')
+
+    
+]
